3a1630bda9d08d9bfd5177747bdf7817a25c0ebb
Anna <toanuta@gmail.com>
2018-11-27 01:29:51 +0300

    Выполнено д/з №1 slider

---
+++ browser/slider/js/javascript.js
@@ -0,0 +1,14 @@
+'use strict';
+var photos = ['airmax-jump.png', 'airmax-on-foot.png', 'airmax-playground.png', 'airmax-top-view.png', 'airmax.png'];
+const slider = document.getElementById('slider');
+let step = 0;
+function showSlider() {
+	if (step === photos.length) {
+		step = 0;
+	}
+	slider.src = `i/${photos[step]}`;
+	step += 1;
+}
+
+setInterval(showSlider, 5000);
+
