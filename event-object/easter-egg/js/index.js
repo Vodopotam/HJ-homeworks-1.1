e93007d7411b57316dac32f89920446adf98c4a4
Anna <toanuta@gmail.com>
2018-12-06 21:05:20 +0300

    Доработано

--- event-object/easter-egg/js/index.js
+++ event-object/easter-egg/js/index.js
@@ -6,20 +6,21 @@ let secretKey = 'KeyY,KeyT,KeyN,KeyJ,KeyK,KeyJ,KeyU,KeyB,KeyZ';
 
 function handleKey() {
 	keyArr.push(event.code);
-	if (keyArr.join().search(secretKey) !== 1) {
+	if (keyArr.join().search(secretKey) !== -1) {
 		secret.classList.add('visible');
 	} else {
 		secret.classList.remove('visible');
 	}
 }
+
 function showNavigtion() {
 	if (event.ctrlKey && event.altKey && event.code === 'KeyT') {
-		navigation.classList.add('visible');
-	} else {
-		navigation.classList.remove('visible');
-	}
+		navigation.classList.toggle('visible');
+	} 
 }
 
 document.addEventListener('keydown', handleKey);
 document.addEventListener('keydown', showNavigtion);
+
+
 
