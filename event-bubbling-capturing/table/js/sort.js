ce6f2671706cab47742a2aa65bec499825f1f652
Anna <toanuta@gmail.com>
2018-12-18 17:27:09 +0300

    Задание выполнено

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,12 @@
 'use strict';
-
 function handleTableClick(event) {
-  
+	if (event.target.tagName === 'TH') {
+		if (event.target.dataset.dir === '1') {
+			event.target.dataset.dir = -1;
+		} else {
+			event.target.dataset.dir = 1;
+		}
+		event.currentTarget.dataset.sortBy = event.target.dataset.propName;
+		sortTable(event.target.dataset.propName, event.target.dataset.dir);
+	}
 }
