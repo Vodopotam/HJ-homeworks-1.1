b75f1dae81551b6759e773a8d29edb463e11d8bd
Anna <toanuta@gmail.com>
2018-12-19 22:24:51 +0300

    Задание выполнено

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,13 @@
 'use strict';
+function checkCartButton(event) {
+	if ((event.target.tagName === 'A') && (event.target.classList.contains('add-to-cart'))){
+		event.preventDefault();
+		let item = {
+			title: event.target.dataset.title,
+			price: event.target.dataset.price
+		};
+		addToCart(item);
+	}
+}
+
+list.addEventListener('click', checkCartButton);
