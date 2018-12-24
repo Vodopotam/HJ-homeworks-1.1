b5caaf08fe791fcba55d5e6a9b5dae16cafa5025
Anna <toanuta@gmail.com>
2018-12-24 14:34:08 +0300

    Задание выполнено

--- local-storage/cart/js/cart.js
+++ local-storage/cart/js/cart.js
@@ -19,12 +19,12 @@ fetch('https://neto-api.herokuapp.com/cart/colors', {
 			}
 
 			colorSwatch.innerHTML += `<div data-value="${item.type}" class="swatch-element color ${item.type} ${available}">
-  				<div class="tooltip">${item.title}</div>
-  				<input quickbeam="color" id="swatch-1-${item.type}" type="radio" name="color" value="${item.type}" ${checked}>
-  				<label for="swatch-1-${item.type}" style="border-color: ${item.type};">
-    			<span style="background-color: ${item.type};"></span>
-    			<img class="crossed-out" src="https://neto-api.herokuapp.com/hj/3.3/cart/soldout.png?10994296540668815886">
-  				</label>
+				<div class="tooltip">${item.title}</div>
+				<input quickbeam="color" id="swatch-1-${item.type}" type="radio" name="color" value="${item.type}" ${checked}>
+				<label for="swatch-1-${item.type}" style="border-color: ${item.type};">
+				<span style="background-color: ${item.type};"></span>
+				<img class="crossed-out" src="https://neto-api.herokuapp.com/hj/3.3/cart/soldout.png?10994296540668815886">
+				</label>
 				</div>`;
 		})
 	});
@@ -48,11 +48,11 @@ fetch('https://neto-api.herokuapp.com/cart/sizes', {
 				checked = 'disabled';
 			}
 			sizeSwatch.innerHTML += `<div data-value="${item.type}" class="swatch-element plain ${item.type} ${available}">
-  				<input id="swatch-0-${item.type}" type="radio" name="size" value="${item.type}" ${checked}>
-  				<label for="swatch-0-${item.type}">
-    			${item.title}
-    			<img class="crossed-out" src="https://neto-api.herokuapp.com/hj/3.3/cart/soldout.png?10994296540668815886">
-  				</label>
+				<input id="swatch-0-${item.type}" type="radio" name="size" value="${item.type}" ${checked}>
+				<label for="swatch-0-${item.type}">
+				${item.title}
+				<img class="crossed-out" src="https://neto-api.herokuapp.com/hj/3.3/cart/soldout.png?10994296540668815886">
+				</label>
 				</div>`
 		})
 		if (localStorage.index) {
@@ -69,22 +69,22 @@ function setBasket(data) {
 	let price = 0;
 	data.forEach(function(item) {
 		cart.innerHTML = `<div class="quick-cart-product quick-cart-product-static" id="quick-cart-product-${item.id}" style="opacity: 1;">
-  			<div class="quick-cart-product-wrap">
-    		<img src="${item.pic}" title="${item.title}">
-    		<span class="s1" style="background-color: #000; opacity: .5">$${item.price}</span>
-    		<span class="s2"></span>
-  			</div>
-  			<span class="count hide fadeUp" id="quick-cart-product-count-${item.id}">${item.quantity}</span>
-  			<span class="quick-cart-product-remove remove" data-id="${item.id}"></span>
+			<div class="quick-cart-product-wrap">
+			<img src="${item.pic}" title="${item.title}">
+			<span class="s1" style="background-color: #000; opacity: .5">$${item.price}</span>
+			<span class="s2"></span>
+			</div>
+			<span class="count hide fadeUp" id="quick-cart-product-count-${item.id}">${item.quantity}</span>
+			<span class="quick-cart-product-remove remove" data-id="${item.id}"></span>
 			</div>`;
 			price = item.price * item.quantity;
 	})
 
 	cart.innerHTML += `<a id="quick-cart-pay" quickbeam="cart-pay" class="cart-ico open">
-  						<span>
-    						<strong class="quick-cart-text">Оформить заказ<br></strong>
-    						<span id="quick-cart-price">$${price}</span>
-  						</span>
+						<span>
+							<strong class="quick-cart-text">Оформить заказ<br></strong>
+							<span id="quick-cart-price">$${price}</span>
+						</span>
 					</a>`;
 
 	const cartPay = document.querySelector('#quick-cart-pay');
