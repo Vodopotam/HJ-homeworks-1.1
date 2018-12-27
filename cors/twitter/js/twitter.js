c016946848c60459374dbc3bf953c751f15dbab2
Anna <toanuta@gmail.com>
2018-12-27 16:02:46 +0300

    Задание выполнено

--- cors/twitter/js/twitter.js
+++ cors/twitter/js/twitter.js
@@ -1,1 +1,23 @@
 'use strict';
+const wallpaper = document.querySelector('[data-wallpaper]'),
+	username = document.querySelector('[data-username]'),
+	description = document.querySelector('[data-description]'),
+	pic = document.querySelector('[data-pic]'),
+	tweets = document.querySelector('[data-tweets]'),
+	followers = document.querySelector('[data-followers]'),
+	following = document.querySelector('[data-following]');
+
+function callback(userData) {
+	wallpaper.src = userData.wallpaper;
+	username.textContent = userData.username;
+	description.textContent = userData.description;
+	pic.src = userData.pic;
+	tweets.value = userData.tweets;
+	followers.value = userData.followers;
+	following.value = userData.following;
+}
+
+const script = document.createElement('script');
+script.setAttribute('src', `https://neto-api.herokuapp.com/twitter/jsonp`);
+document.body.appendChild(script);
+
